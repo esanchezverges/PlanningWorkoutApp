1d1873982de86b9254be117d97f02397ab01e5f8
Eric Sanchez <esanchezverges@gmail.com>
2025-06-24 17:02:07 +0200

    Added input component form workout WIP

--- PlanningWorkoutApp.Web/Program.cs
+++ PlanningWorkoutApp.Web/Program.cs
@@ -1,10 +1,12 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PlanningWorkoutApp.Web;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+builder.Services.AddRadzenComponents();
 
 builder.Services.AddScoped(sp =>
     new HttpClient { BaseAddress = new Uri("https://esanchezverges.github.io/PlanningWorkoutApp/") }
