702b877a49f454e8d6ac7276e1af4592bc35233d
Eric Sanchez Verges <esanchezverges@gmail.com>
2025-06-27 16:00:37 +0200

    Add exercises to the page with the dialog

---
+++ PlanningWorkoutApp.Web/Components/TestClasses.cs
@@ -0,0 +1,20 @@
+﻿namespace PlanningWorkoutApp.Web;
+
+
+public class ExerciseData
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string BodyPart { get; set; }
+    public string Equipment { get; set; }
+    public string TargetMuscle { get; set; }
+    public string ImageUrl { get; set; }
+    public string VideoUrl { get; set; }
+}
+public class ExerciseInWorkout
+{
+    public string Name { get; set; }
+    public int Repetitions { get; set; }
+    public double Weight { get; set; }
+}
+
